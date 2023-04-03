a15c6461ff74dcc8cefc5348c92be6af1039fcb3
Mubeen31 <qs6272527@gmail.com>
2023-04-03 20:34:30 +0100

    make it better

--- data.py
+++ data.py
@@ -1,3 +1,9 @@
 config = {
-
+  "apiKey": "AIzaSyB53kiVz_8ttRQLhfdP3F9W83imJEauXHA",
+  "authDomain": "crudapp-29808.firebaseapp.com",
+  "databaseURL": "https://crudapp-29808-default-rtdb.firebaseio.com",
+  "projectId": "crudapp-29808",
+  "storageBucket": "crudapp-29808.appspot.com",
+  "messagingSenderId": "856750806825",
+  "appId": "1:856750806825:web:9167dc83c24a9deb92c938"
 }
